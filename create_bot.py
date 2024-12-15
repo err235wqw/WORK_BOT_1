e923cec181b103c5f99b55518a68e5645dc7649d
Vyacheslav Sinew <vyacheslav.sinew@yandex.ru>
2024-12-15 23:48:22 +0400

    2024-12-15 Хот фикс

--- create_bot.py
+++ create_bot.py
@@ -1,9 +1,11 @@
 from os import getenv
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
+from dotenv import load_dotenv
 
 storage = MemoryStorage()
 
+load_dotenv()
 token = getenv('TG_TOKEN')
 bot = Bot(token=token)
 dp = Dispatcher()
