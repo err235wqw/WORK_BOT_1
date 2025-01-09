39c617a10ecc9b0debdfc2329b8cd27769d749e7
Вячеслав Синёв <vyacheslav.sinew@yandex.ru>
2025-01-09 04:48:25 +0400

    2025-01-09 Добавил темы, поправил генерацию, добавил рекламу, обновил группу

--- handlers/client.py
+++ handlers/client.py
@@ -14,51 +14,79 @@ import app.feature as ft
 
 
 async def command_start(message: Message, state: FSMContext):
-    sub = await bot.get_chat_member(chat_id='@testnew29', user_id=message.from_user.id)
+    sub = await bot.get_chat_member(chat_id='@hypnosis_language', user_id=message.from_user.id)
     if sub.status == 'left':
-        await message.answer(f'Подпишитесь на телеграмм канал:\nhttps://t.me/testnew29', reply_markup=ReplyKeyboardRemove())
+        await message.answer(f'Подпишитесь на телеграмм канал:\nhttps://t.me/hypnosis_language', reply_markup=ReplyKeyboardRemove())
     else:
         await message.answer(src.start_phrase, reply_markup=kb.ChooseKoloda)
         await state.set_state(st.start_state.start)
 
 
 async def command_help(message: Message):
-    sub = await bot.get_chat_member(chat_id='@testnew29', user_id=message.from_user.id)
+    sub = await bot.get_chat_member(chat_id='@hypnosis_language', user_id=message.from_user.id)
     if sub.status == 'left':
-        await message.answer(f'Подпишитесь на телеграмм канал:\nhttps://t.me/testnew29', reply_markup=ReplyKeyboardRemove())
+        await message.answer(f'Подпишитесь на телеграмм канал:\nhttps://t.me/hypnosis_language', reply_markup=ReplyKeyboardRemove())
     else:
         await message.answer('Вы попросили помощь, хз, можете помолиться')
 
 
 # region Start
-async def start_generate_standart(message: Message, state: FSMContext):
-    sub = await bot.get_chat_member(chat_id='@testnew29', user_id=message.from_user.id)
+async def post_start_generate(message: Message, state: FSMContext):
+    sub = await bot.get_chat_member(chat_id='@hypnosis_language', user_id=message.from_user.id)
     if sub.status == 'left':
-        await message.answer(f'Подпишитесь на телеграмм канал:\nhttps://t.me/testnew29', reply_markup=ReplyKeyboardRemove())
+        await message.answer(f'Подпишитесь на телеграмм канал:\nhttps://t.me/hypnosis_language', reply_markup=ReplyKeyboardRemove())
     else:
-        await message.answer(src.choose_phrase_start, reply_markup=ReplyKeyboardRemove())
+        data = await state.get_data()
+        Da = data.get('Da', False)
+        await state.set_state(st.start_state.post_start)
+        bll = False
+        if message.text in ['Переговоры','Продажи','Отношения между мужчиной и женщиной','Отношения между родителями и ребенком']:
+            await state.update_data(post_start=message.text)
+            bll = True
+        if bll or message.text == 'Продолжить со случайной темой':
+            if not bll:
+                await state.set_state(st.start_state.proverka)
+                await state.update_data(proverka = False)
+            else:
+                await state.set_state(st.start_state.proverka)
+                await state.update_data(proverka = True)
+            if Da:
+                await message.answer(src.choose_phrase_da, reply_markup=ReplyKeyboardRemove())
+            else:
+                await message.answer(src.choose_phrase_start, reply_markup=ReplyKeyboardRemove())
+            await state.set_state(st.start_state.choose)
+        else:
+            await state.set_state(st.start_state.post_start)
+            await message.answer(src.choose_theme, reply_markup=kb.ChooseTheme)
+
+async def start_generate_standart(message: Message, state: FSMContext):
+    sub = await bot.get_chat_member(chat_id='@hypnosis_language', user_id=message.from_user.id)
+    if sub.status == 'left':
+        await message.answer(f'Подпишитесь на телеграмм канал:\nhttps://t.me/hypnosis_language', reply_markup=ReplyKeyboardRemove())
+    else:
+        await message.answer(src.choose_theme, reply_markup=kb.ChooseTheme)
         await state.set_state(st.start_state.Da)
         await state.update_data(Da=False)
-        await state.set_state(st.start_state.choose)
+        await state.set_state(st.start_state.post_start)
 
 
 async def start_generate_da(message: Message, state: FSMContext):
-    sub = await bot.get_chat_member(chat_id='@testnew29', user_id=message.from_user.id)
+    sub = await bot.get_chat_member(chat_id='@hypnosis_language', user_id=message.from_user.id)
     if sub.status == 'left':
-        await message.answer(f'Подпишитесь на телеграмм канал:\nhttps://t.me/testnew29', reply_markup=ReplyKeyboardRemove())
+        await message.answer(f'Подпишитесь на телеграмм канал:\nhttps://t.me/hypnosis_language', reply_markup=ReplyKeyboardRemove())
     else:
-        await message.answer(src.choose_phrase_da, reply_markup=ReplyKeyboardRemove())
+        await message.answer(src.choose_theme, reply_markup=kn.ChooseTheme)
         await state.set_state(st.start_state.Da)
         await state.update_data(Da=True)
-        await state.set_state(st.start_state.choose)
+        await state.set_state(st.start_state.post_start)
 # endregion
 
 
 # region training
 async def start_generate(message: Message, state: FSMContext):
-    sub = await bot.get_chat_member(chat_id='@testnew29', user_id=message.from_user.id)
+    sub = await bot.get_chat_member(chat_id='@hypnosis_language', user_id=message.from_user.id)
     if sub.status == 'left':
-        await message.answer(f'Подпишитесь на телеграмм канал:\nhttps://t.me/testnew29', reply_markup=ReplyKeyboardRemove())
+        await message.answer(f'Подпишитесь на телеграмм канал:\nhttps://t.me/hypnosis_language', reply_markup=ReplyKeyboardRemove())
     else:
         data = await state.get_data()
         Da = data.get('Da', False)
@@ -75,20 +103,28 @@ async def start_generate(message: Message, state: FSMContext):
         theme_number = await ft.generate_random_number(99)
         await state.set_state(st.start_state.theme_number)
         await state.update_data(theme_number=theme_number)
+# Можно убрать генерацию если без проверки
+        await state.set_state(st.start_state.post_number)
+        post_number = await ft.generate_random_number(len(src.categories[data.get('post_start', 'Продажи')])-1)
+        await state.update_data(post_number=post_number)
+
         await state.set_state(st.start_state.list_of_phrases)
         await state.update_data(list_of_phrases=phrases)
         first = await ft.generate_random_number(len(phrases)-1)
         await state.set_state(st.start_state.cur)
         await state.update_data(cur=first)
-        await message.answer(src.problems[theme_number], reply_markup=None)
+        if data.get('proverka', True):
+            await message.answer(src.categories[data.get('post_start', 'Продажи')][post_number], reply_markup=None)
+        else:
+            await message.answer(src.problems[theme_number], reply_markup=None)
         await message.answer(phrases[first][1], reply_markup=kb.TipKb)
         await state.set_state(st.start_state.train)
 
 
 async def training(message: Message, state: FSMContext):
-    sub = await bot.get_chat_member(chat_id='@testnew29', user_id=message.from_user.id)
+    sub = await bot.get_chat_member(chat_id='@hypnosis_language', user_id=message.from_user.id)
     if sub.status == 'left':
-        await message.answer(f'Подпишитесь на телеграмм канал:\nhttps://t.me/testnew29', reply_markup=ReplyKeyboardRemove())
+        await message.answer(f'Подпишитесь на телеграмм канал:\nhttps://t.me/hypnosis_language', reply_markup=ReplyKeyboardRemove())
     else:
         data = await state.get_data()
         await state.set_state(st.start_state.prev)
@@ -98,19 +134,28 @@ async def training(message: Message, state: FSMContext):
             number = await ft.generate_random_number(len(data['list_of_phrases'])-1)
         await state.set_state(st.start_state.cur)
         await state.update_data(cur=number)
-        await message.answer(src.problems[data.get('theme_number', 0)], reply_markup=None)
+        if data.get('proverka', True):
+            await message.answer(src.categories[data.get('post_start', 'Продажи')][post_number], reply_markup=None)
+        else:
+            await message.answer(src.problems[theme_number], reply_markup=None)
         await message.answer(data['list_of_phrases'][number][1], reply_markup=kb.TipKb)
         await state.set_state(st.start_state.train)
 
 
 async def heandler_callback(call: CallbackQuery, state: FSMContext):
-    sub = await bot.get_chat_member(chat_id='@testnew29', user_id=call.message.from_user.id)
+    sub = await bot.get_chat_member(chat_id='@hypnosis_language', user_id=call.message.from_user.id)
     if sub.status == 'left':
-        await call.message.answer(f'Подпишитесь на телеграмм канал:\nhttps://t.me/testnew29', reply_markup=ReplyKeyboardRemove())
+        await call.message.answer(f'Подпишитесь на телеграмм канал:\nhttps://t.me/hypnosis_language', reply_markup=ReplyKeyboardRemove())
     else:
+        advert = src.generate_add()
+        if(advert!=''):
+            await call.message.answer(advert, reply_markup=None)
         data = await state.get_data()
         if call.data == 'tip':
-            await call.message.answer(src.problems[data.get('theme_number', 0)], reply_markup=None)
+            if data.get('proverka', True):
+                await message.answer(src.categories[data.get('post_start', 'Продажи')][post_number], reply_markup=None)
+            else:
+                await message.answer(src.problems[theme_number], reply_markup=None)
             await call.message.answer(data['list_of_phrases'][data['cur']][2], reply_markup=kb.Next)
             await call.answer()
         elif call.data == 'next':
@@ -121,7 +166,11 @@ async def heandler_callback(call: CallbackQuery, state: FSMContext):
                 number = await ft.generate_random_number(len(data['list_of_phrases'])-1)
             await state.set_state(st.start_state.cur)
             await state.update_data(cur=number)
-            await call.message.answer(src.problems[data.get('theme_number', 0)], reply_markup=None)
+
+            if data.get('proverka', True):
+                await message.answer(src.categories[data.get('post_start', 'Продажи')][post_number], reply_markup=None)
+            else:
+                await message.answer(src.problems[theme_number], reply_markup=None)
             await call.message.answer(data['list_of_phrases'][number][1], reply_markup=kb.TipKb)
             await call.answer()
             await state.set_state(st.start_state.train)
@@ -129,12 +178,17 @@ async def heandler_callback(call: CallbackQuery, state: FSMContext):
             await call.message.answer(src.start_phrase, reply_markup=kb.ChooseKoloda)
             await state.set_state(st.start_state.start)
         elif call.data == 'change_theme':
-            theme_number = data.get('theme_number', 0)
-            while theme_number == data.get('theme_number', 0):
-                theme_number = await ft.generate_random_number(99)
-            await state.set_state(st.start_state.theme_number)
-            await state.update_data(theme_number=theme_number)
-
+            if data.get('proverka', True):
+                theme_number = data.get('theme_number', 0)
+                while theme_number == data.get('theme_number', 0):
+                    theme_number = await ft.generate_random_number(99)
+                await state.set_state(st.start_state.theme_number)
+                await state.update_data(theme_number=theme_number)
+            else:
+                await state.set_state(st.start_state.post_number)
+            post_number = await ft.generate_random_number(len(src.categories[data.get('post_start', 'Продажи')])-1)
+            await state.update_data(post_number=post_number)
+            
             await state.set_state(st.start_state.prev)
             await state.update_data(prev=data["cur"])
             number = data['cur']
@@ -143,7 +197,11 @@ async def heandler_callback(call: CallbackQuery, state: FSMContext):
             await state.set_state(st.start_state.cur)
             await state.update_data(cur=number)
             await call.message.answer(src.new_theme, reply_markup=None)
-            await call.message.answer(src.problems[theme_number], reply_markup=None)
+            # Проверка
+            if data.get('proverka', True):
+                await message.answer(src.categories[data.get('post_start', 'Продажи')][post_number], reply_markup=None)
+            else:
+                await message.answer(src.problems[theme_number], reply_markup=None)
             await call.message.answer(data['list_of_phrases'][number][1], reply_markup=kb.TipKb)
             await call.answer()
             await state.set_state(st.start_state.train)
@@ -159,6 +217,7 @@ def register_handlers_client(dp: Dispatcher):
                         F.text == 'Стандартные колоды')
     dp.message.register(start_generate_da, st.start_state.start,
                         F.text == 'Колоды "ДА"')
+    dp.message.register(post_start_generate, st.start_state.start, st.start_state.post_start, F.text)
     dp.message.register(
         start_generate, st.start_state.choose, F.text)
     dp.message.register(training, st.start_state.train,
